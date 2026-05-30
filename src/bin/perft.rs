6621e6241123962d99bbcdc8a173335977204864
tuasananh <tuasananh@gmail.com>
2026-05-30 20:44:04 +0700

    feat(profiling): add search binary for profiling

--- src/bin/perft.rs
+++ src/bin/perft.rs
@@ -4,9 +4,15 @@ use lingine::benchmark::Perft;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    /// Board position in FEN notation.
+    #[arg(
+        short,
+        long,
+        default_value = "rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR w - - 0 1"
+    )]
     fen: String,
-    #[arg(short, long)]
+
+    #[arg(short, long, default_value_t = 5)]
     depth: u32,
 }
 
