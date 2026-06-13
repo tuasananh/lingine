96e14f0482d204e8a8c0fd814f453c4ac3d36e10
tuasananh <tuasananh@gmail.com>
2026-06-13 14:37:20 +0700

    perf: skip one piece to make color() and piece_type() faster

--- src/core/types/pieces.rs
+++ src/core/types/pieces.rs
@@ -14,41 +14,28 @@ pub enum PieceType {
 /// Represents standard Xiangqi pieces, categorized by color and piece type.
 /// Red pieces are represented by values 1-7, and Black pieces by values 9-15.
 #[rustfmt::skip]
-#[derive(FromRepr, EnumCount, EnumIter, Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(FromRepr, EnumIter, Clone, Copy, PartialEq, Eq, Debug)]
 #[repr(u8)]
 pub enum Piece {
-    RedRook, RedAdvisor, RedCannon, RedPawn, RedKnight, RedBishop, RedKing,
-    BlackRook, BlackAdvisor, BlackCannon, BlackPawn, BlackKnight, BlackBishop, BlackKing,
+    RedRook = 0, RedAdvisor, RedCannon, RedPawn, RedKnight, RedBishop, RedKing,
+    BlackRook = 8, BlackAdvisor, BlackCannon, BlackPawn, BlackKnight, BlackBishop, BlackKing,
+}
+
+impl Piece {
+    pub const COUNT: usize = Self::BlackKing as usize + 1;
 }
 
 impl Piece {
     /// Extracts the `Color` of the piece, or returns `None` if it is `None`.
     #[inline]
     pub const fn color(&self) -> Side {
-        match *self {
-            Piece::RedRook
-            | Piece::RedAdvisor
-            | Piece::RedCannon
-            | Piece::RedPawn
-            | Piece::RedKnight
-            | Piece::RedBishop
-            | Piece::RedKing => Side::Red,
-            _ => Side::Black,
-        }
+        Side::from_repr((*self as u8) >> 3).unwrap()
     }
 
     /// Extracts the `PieceType` of the piece.
     #[inline]
     pub const fn piece_type(&self) -> PieceType {
-        match *self {
-            Piece::RedRook | Piece::BlackRook => PieceType::Rook,
-            Piece::RedAdvisor | Piece::BlackAdvisor => PieceType::Advisor,
-            Piece::RedCannon | Piece::BlackCannon => PieceType::Cannon,
-            Piece::RedPawn | Piece::BlackPawn => PieceType::Pawn,
-            Piece::RedKnight | Piece::BlackKnight => PieceType::Knight,
-            Piece::RedBishop | Piece::BlackBishop => PieceType::Bishop,
-            Piece::RedKing | Piece::BlackKing => PieceType::King,
-        }
+        PieceType::from_repr((*self as u8) & 7).unwrap()
     }
 }
 
